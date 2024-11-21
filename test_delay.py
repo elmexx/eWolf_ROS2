d2a54a3e43ed55c6884f637c6ff1fff861691770
elmexx <gaokunbit@hotmail.com>
2024-11-21 14:32:33 +0100

    Update test_delay.py

--- test_delay.py
+++ test_delay.py
@@ -59,3 +59,57 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
+# multi topic delay
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image as SensorImage
+from std_msgs.msg import Header
+from builtin_interfaces.msg import Time
+from message_filters import Subscriber, ApproximateTimeSynchronizer
+from lane_parameter_msg.msg import LaneParams
+
+class MultiTopicDelayCalculator(Node):
+    def __init__(self):
+        super().__init__('multi_topic_delay_calculator')
+
+        self.system_time_sub = Subscriber(self, Time, 'system_time')
+        self.camera_image_sub = Subscriber(self, SensorImage, '/camera/color/image_raw')
+        self.lane_info_sub = Subscriber(self, LaneParams, 'leftlanedetection')
+        
+        self.sync = ApproximateTimeSynchronizer(
+            [self.system_time_sub, self.camera_image_sub, self.lane_info_sub],
+            queue_size=10,
+            slop=0.1 
+        )
+        self.sync.registerCallback(self.sync_callback)
+
+    def sync_callback(self, system_time_msg, camera_msg, lane_msg):
+
+        system_time = rclpy.time.Time(seconds=system_time_msg.sec, nanoseconds=system_time_msg.nanosec)
+        camera_time = rclpy.time.Time.from_msg(camera_msg.header.stamp)
+        lane_time = rclpy.time.Time.from_msg(lane_msg.stamp)
+
+        delay_camera_to_system = (camera_time - system_time).nanoseconds / 1e6  
+        delay_lane_to_system = (lane_time - system_time).nanoseconds / 1e6  
+        delay_camera_to_lane = (camera_time - lane_time).nanoseconds / 1e6  
+
+        self.get_logger().info(f'Delay (Camera Image-> System): {delay_camera_to_system:.3f} ms')
+        self.get_logger().info(f'Delay (Lane -> System): {delay_lane_to_system:.3f} ms')
+        self.get_logger().info(f'Delay (Speedgoat -> Lane): {delay_camera_to_lane:.3f} ms')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MultiTopicDelayCalculator()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+# speedgoat udp add system time
+import time
+current_time = time.time()
+combined_array = np.concatenate((udp_data, [current_time]), axis=0).astype(np.float64)
